6c600155ab390f2161e33bace367a05a69491a7e
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-05-17 15:29:35 +0100

    Adding animation on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -48,4 +48,41 @@ $(document).ready(function () {
       }
     });
   });
+
+  // Animations on scroll
+  $('.js--wp-1').waypoint(
+    function (_) {
+      $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-2').waypoint(
+    function (_) {
+      $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-3').waypoint(
+    function (_) {
+      $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    },
+    {
+      offset: '50%',
+    }
+  );
+
+  $('.js--wp-4').waypoint(
+    function (_) {
+      $('.js--wp-4').addClass('animate__animated animate__pulse');
+    },
+    {
+      offset: '50%',
+    }
+  );
 });
