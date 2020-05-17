8f98793fbacccc3deca1966bd7baef7712e6772c
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-05-17 14:29:04 +0100

    Scrolling to elements

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,51 @@
 $(document).ready(function () {
-  $(".js--section-features").waypoint(
+  // Sticky navigation
+  $('.js--section-features').waypoint(
     function (direction) {
-      if (direction === "down") {
-        $("nav").addClass("sticky-nav");
+      if (direction === 'down') {
+        $('nav').addClass('sticky-nav');
       } else {
-        $("nav").removeClass("sticky-nav");
+        $('nav').removeClass('sticky-nav');
       }
     },
     {
-      offset: "60px",
+      offset: '60px',
     }
   );
+
+  // Scroll on buttons
+  $('.js--scroll-to-plan').click(function () {
+    $('html, body').animate(
+      { scrollTop: $('.js--section-plans').offset().top },
+      1000
+    );
+  });
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate(
+      { scrollTop: $('.js--section-features').offset().top },
+      1000
+    );
+  });
+
+  // Navigation scroll
+  $(function () {
+    $('a[href*=\\#]:not([href=\\#])').click(function () {
+      if (
+        location.pathname.replace(/^\//, '') ===
+          this.pathname.replace(/^\//, '') &&
+        location.hostname === this.hostname
+      ) {
+        let target = $(this.hash);
+        target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+        if (target.length) {
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+        }
+      }
+    });
+  });
 });
